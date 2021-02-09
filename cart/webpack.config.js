4324edab8f137d17f4817e408343c06c6335b626
DevAlberto <devalbertobarboza@gmail.com>
2021-02-09 16:55:12 -0300

    section 3 finished

--- cart/webpack.config.js
+++ cart/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
             name: 'cart',
             filename: 'remoteEntry1.js',
             exposes: {
-                './CartShow': './src/index'
+                './CartShow': './src/bootstrap'
             },
             shared: ['faker'],
         }),
--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,5 +1,36 @@
 import faker from 'faker';
 
-const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
 
-document.querySelector('#products-dev').innerHTML = cartText;
+const mount = (el) => {
+    let products = '';
+
+    for(let i = 0; i < 5; i++) {
+        const name = faker.commerce.productName();
+        products += `<div>${name}</div>`
+    }
+
+    el.innerHTML = products;
+}
+
+// Context-Situation #1
+// We are running this file in development in isolation
+// We are using our local index.html file
+// Wich DEFNITELY has an element with an id of 'products-dev
+// We want to immidiately render our app into that element
+if (process.env.NODE_ENV === 'development') {
+    const el = document.querySelector('#products-dev');
+
+    //Asumin  our container doesnt have an element with id 'dev-products'
+    if (el) {
+        //we are probabli running in isolation
+        mount(el)
+    }
+}
+
+// Context/Situacion #2
+// We are running this file in  development or production
+// throught the container app
+// No GARANTUEE that an element with an id of 'products-dev'
+// WE DO NOT WANT try to immidiately render the app
+
+export { mount };
